19b25cf12251480ca0db15dd259712c1181ce456
Asim Poudel <asimdkt63@gmail.com>
2025-06-05 13:46:59 +0545

    Update bisection.py

--- lab1/bisection.py
+++ lab1/bisection.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-f=lambda x: x**3-(x)-2
+f=lambda x: x**3-5*(x)-2
 
 # for i in range(10):
 #     print(i,f(i))
@@ -14,8 +14,8 @@ ax.plot(x,y)
 
 # f(x) is continuous at interval [1,2]
 
-a=1
-b=2
+a=0
+b=-1
 error=0.00001
 while (abs(b-a)>error):
     c=(a+b)/2
