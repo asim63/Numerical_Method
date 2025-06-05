f42c6fe281c833bfc52f357105774e18a952f8b9
Asim Poudel <asimdkt63@gmail.com>
2025-06-05 13:58:02 +0545

    Update newton-raphson.py

--- lab2/newton-raphson.py
+++ lab2/newton-raphson.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x): return x * np.sin(x) + np.cos(x) 
+def f(x): return x * np.sin(x) + x  + np.cos(x) 
 def g(x): return x * np.cos(x)
 
 fig, ax = plt.subplots()
@@ -10,7 +10,7 @@ y=f(x)
 ax.plot(x,y,label="f(x)=xsin(x)+cos(x)")
 
 # the function is continuous in 2 & 3
-a=2.5
+a=4.5
 b=a-(f(a)/g(a))
 error=0.00001
 while (abs((b-a)/b)>error):
@@ -21,7 +21,7 @@ print("Real root:",b)
 ax.set_title("Newton rapshon Method")
 ax.set_xlabel('x')
 ax.set_ylabel('f(x)')
-ax.scatter(b,0,color='red',marker='x', label=f"Approx Root={b}")
+ax.scatter(b,0,color='red',marker='o', label=f"Approx Root={b}")
 plt.legend()
 ax.grid()
 plt.show()
