c48d523549e56549daad20553037213617daa236
Asim Poudel <asimdkt63@gmail.com>
2025-06-11 20:52:36 +0545

    Update newton_forward.py

--- lab2/newton_forward.py
+++ lab2/newton_forward.py
@@ -9,7 +9,6 @@ d[:, 0] = y_init
 for i in range(1, n):
     for j in range(n - i):
         d[j][i] = d[j + 1][i - 1] - d[j][i - 1]
-
 print("Forward Difference Table:")
 for i in range(n):
     print(f"{x_vals[i]:.2f}", end="\t")
@@ -26,3 +25,12 @@ for i in range(1,n):
     P*=(p-i+1)
     value += (P*d[0][i])/math.factorial(i)
 print(f"\nInterpolated value at x = {x} is {value}")
+fig, ax = plt.subplots()
+ax.set_title('Newton Forward Difference Interpolation')
+ax.set_xlabel('x')  
+ax.set_ylabel('f(x)')
+ax.plot(x_vals, y_init, 'bo-', label='Original Data')
+ax.scatter(x, value, color='red', marker='o', label=f"Interpolated Value at x={x}")
+ax.legend()
+plt.grid(True)
+plt.show()
